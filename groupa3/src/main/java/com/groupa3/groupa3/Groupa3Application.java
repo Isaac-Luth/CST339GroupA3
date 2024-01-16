8db1dbec7a90b7dca40117a581c4c8de8d5128a8
Isaac-Luth <isaacluth@gmail.com>
2024-01-15 18:34:19 -0700

    init

---
+++ groupa3/src/main/java/com/groupa3/groupa3/Groupa3Application.java
@@ -0,0 +1,13 @@
+package com.groupa3.groupa3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groupa3Application.class, args);
+	}
+
+}
