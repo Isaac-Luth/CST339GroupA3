8aed2a12d7cd2ca3854c7acce569bd23a5ef7008
Isaac Luth <isaacluth@gmail.com>
2024-01-30 13:35:05 -0700

    Update VerifyInputs.java

--- groupa3/src/main/java/VerifyInputs.java
+++ groupa3/src/main/java/VerifyInputs.java
@@ -3,23 +3,23 @@ import java.util.regex.Pattern;
 
 public class VerifyInputs {
 
-    //isEmail method is used to verify that the input is indeed an email
-    public boolean isValidEmail(String input){
-        //Define a regular expression pattern for a simple email validation
-        //this may not cover all email cases but should handle most
-        String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+	//isEmail method is used to verify that the input is indeed an email
+	public boolean isValidEmail(String input){
+	//Define a regular expression pattern for a simple email validation
+	//this may not cover all email cases but should handle most
+	String emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+	
+	//Compile the pattern
+	Pattern pattern = Pattern.compile(emailPattern);
+	
+	//Use the pattern to create a Matcher object
+	Matcher matcher = pattern.matcher(input);
+	
+	//If it is a match, it will return true, else it will return false
+	return matcher.matches();
+	}
 
-        //Compile the pattern
-        Pattern pattern = Pattern.compile(emailPattern);
-
-        //Use the pattern to create a Matcher object
-        Matcher matcher = pattern.matcher(input);
-
-        //If it is a match, it will return true, else it will return false
-        return matcher.matches();
-    }
-
-    // Method to verify if the phone number entered is a true phone number
+	// Method to verify if the phone number entered is a true phone number
 	public boolean isValidPhoneNumber(String input)
 	{
 		// Phone number pattern
@@ -36,5 +36,5 @@ public class VerifyInputs {
 		return (m.matches());
 	}
     
-    //Add the other methods
+	//Add the other methods
 }
